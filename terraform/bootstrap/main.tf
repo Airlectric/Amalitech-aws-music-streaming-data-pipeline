486ae3c6931ac80898f8df479e92f6db721ced21
Daniel Agudey Doe <agudeydaniel8@gmail.com>
2026-05-24 13:17:42 +0000

    fix: add empty filter to lifecycle rule to suppress warning

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -98,6 +98,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "state" {
     id     = "expire-noncurrent-versions"
     status = "Enabled"
 
+    filter {}
+
     noncurrent_version_expiration {
       noncurrent_days = 90
     }
